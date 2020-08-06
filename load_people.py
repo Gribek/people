a3bfa0f1af259b6e9e24d1073c49c40a9b52e909
Gribek <piotrekmaslewski@gmail.com>
2020-08-06 21:44:14 +0200

    Add new class to modify data received from API
    Class constructor and methods to read and overwrite values in API data dictionary.

--- load_people.py
+++ load_people.py
@@ -24,3 +24,36 @@ class ApiDataDownloader:
             self.response = True
             if response.status == 200:
                 self.data = json.loads(response.read())
+
+
+class ApiDataModifier:
+    """Modify data received from API."""
+
+    def __init__(self, downloader, modifications, data_dict=None):
+        self.__data = downloader.data
+        if data_dict is not None:
+            self.__data = self.__data[data_dict]
+        self.__modifications = modifications
+
+    @staticmethod
+    def get_value(dict_obj, key_path):
+        """Get the value from the dictionary."""
+        result = dict_obj
+        for key in key_path:
+            try:
+                result = result[key]
+            except KeyError:
+                return None
+        return result
+
+    @staticmethod
+    def set_value(dict_obj, key_path, value):
+        """Set the value in the dictionary."""
+        temp = dict_obj
+        for i in range(len(key_path) - 1):
+            try:
+                temp = temp[key_path[i]]
+            except KeyError:
+                return False
+        temp[key_path[-1]] = value
+        return True
