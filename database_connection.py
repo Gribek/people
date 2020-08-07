6abed53c2516f39e281983c934fbdceda826d7ae
Gribek <piotrekmaslewski@gmail.com>
2020-08-07 15:06:28 +0200

    Remove autoconection argument - connection errors

--- database_connection.py
+++ database_connection.py
@@ -3,6 +3,5 @@ from peewee import SqliteDatabase
 
 def sqlite_connection(filename):
     """Establish a connection to the SQLite database"""
-    cnx = SqliteDatabase(filename, autoconnect=False,
-                         pragmas={'foreign_keys': 1})
+    cnx = SqliteDatabase(filename, pragmas={'foreign_keys': 1})
     return cnx
