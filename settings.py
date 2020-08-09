e06d481e5b3517955ee4f28073d23d1eeb8544ad
Gribek <piotrekmaslewski@gmail.com>
2020-08-09 13:35:18 +0200

    Create settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Name of the database file
+DATABASE = 'people.db'
+
+# Url address to the randomuser API
+API_URL = 'https://randomuser.me/api/?'
+
+# Parameters used when a request is made to the randomuser API
+API_PARAMETERS = {'results': 1000, 'seed': 'abc'}
