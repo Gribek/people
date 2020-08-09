9d6900efb12b74c88ca942ccd2f8cb56e440e575
Gribek <piotrekmaslewski@gmail.com>
2020-08-09 20:19:45 +0200

    Add tests for ApiDataModifier methods

--- test_people.py
+++ test_people.py
@@ -1,13 +1,16 @@
+import datetime
+import re
+
 import pytest
 
 from functions import password_score
-from load_people import ApiDataDownloader, ApiDataReader
+from load_people import ApiDataDownloader, ApiDataReader, ApiDataModifier
 from settings import API_URL
 
 
 @pytest.fixture
 def downloader_obj():
-    api_param = {'results': 5, 'seed': 'abc', }
+    api_param = {'results': 1, 'seed': 'abc', }
     downloader = ApiDataDownloader(API_URL, api_param)
     return downloader
 
@@ -17,6 +20,20 @@ def reader_obj(downloader_obj):
     downloader_obj.send_request()
     reader = ApiDataReader(downloader_obj, data_location='results')
     return reader
+
+
+@pytest.fixture
+def modifier_obj(downloader_obj):
+    downloader_obj.send_request()
+    modifications = (
+        {'name': 'remove_non_digit', 'key_path': ('phone',)},
+        {'name': 'remove_non_digit', 'key_path': ('cell',)},
+        {'name': 'days_to_birthday', 'key_path': ('dob', 'days_to_birthday',)},
+        {'name': 'delete_value', 'key_path': ('picture',)},
+    )
+    modifier = ApiDataModifier(
+        downloader_obj, modifications, data_dict='results')
+    return modifier
 
 
 class TestApiDataDownloader:
@@ -48,8 +65,10 @@ class TestApiDataReader:
         phone = dict_obj['phone']
         firstname = dict_obj['name']['first']
         street = dict_obj['location']['street']['name']
+
         error_value = 'Incorrect value returned'
         error_key_error = 'Value not None when key does not exist'
+
         assert reader_obj.get_value(dict_obj, ('phone',)) == phone, error_value
         assert reader_obj.get_value(
             dict_obj, ('name', 'first')) == firstname, error_value
@@ -57,6 +76,69 @@ class TestApiDataReader:
             dict_obj, ('location', 'street', 'name')) == street, error_value
         assert reader_obj.get_value(
             dict_obj, ('non-existent_key',)) is None, error_key_error
+
+
+class TestApiDataModifier:
+
+    def test_api_data_modifier_set_value(self, modifier_obj):
+        dict_obj = modifier_obj._data[0]
+        phone = '000-000-000'
+        firstname = 'Some non-existing name'
+        street = 'Name of the street that does not exist'
+        new_value = 'new_value'
+        value_returned = modifier_obj.set_value(dict_obj, ('phone',), phone)
+        modifier_obj.set_value(dict_obj, ('name', 'first'), firstname)
+        modifier_obj.set_value(
+            dict_obj, ('location', 'street', 'name'), street)
+        modifier_obj.set_value(dict_obj, ('new_key',), new_value)
+
+        error_value = 'Value not set'
+        error_new_key_value = 'New key-value not set'
+        error_true_returned = 'True not returned after setting value'
+        error_key_error = 'False not returned when key path is incorrect'
+
+        assert dict_obj['phone'] == phone, error_value
+        assert dict_obj['name']['first'] == firstname, error_value
+        assert dict_obj['location']['street']['name'] == street, error_value
+        assert dict_obj['new_key'] == new_value, error_new_key_value
+        assert modifier_obj.set_value(dict_obj, ('phone',), phone)
+        assert value_returned is True, error_true_returned
+        assert modifier_obj.set_value(
+            dict_obj, ('non-existent_key', 'street'),
+            street) is False, error_key_error
+
+    def test_api_data_modifier_delete_value(self, modifier_obj):
+        dict_obj = modifier_obj._data[0]
+        resp = modifier_obj.delete_value(dict_obj, ('email',))
+        modifier_obj.delete_value(dict_obj, ('name', 'first'))
+        modifier_obj.delete_value(dict_obj, ('location', 'street', 'name'))
+
+        error_value = 'Value not deleted'
+        error_true_returned = 'True not returned after deleting value'
+        error_key_error = 'False not returned when key path is incorrect'
+
+        assert 'email' not in dict_obj, error_value
+        assert 'first' not in dict_obj['name'], error_value
+        assert 'name' not in dict_obj['location']['street'], error_value
+        assert resp is True, error_true_returned
+        assert modifier_obj.delete_value(
+            dict_obj, ('non-existent_key',)) is False, error_key_error
+        assert modifier_obj.delete_value(
+            dict_obj, ('non-existent_key', 'street')) is False, error_key_error
+
+    def test_remove_non_digit(self, modifier_obj):
+        dict_obj = modifier_obj._data[0]
+        modifier_obj.remove_non_digit(dict_obj, ('phone',))
+        modifier_obj.remove_non_digit(dict_obj, ('cell',))
+        error = 'Non-digit characters present'
+        assert bool(re.search(r'\D', dict_obj['phone'])) is False, error
+        assert bool(re.search(r'\D', dict_obj['cell'])) is False, error
+
+    def test_days_to_birthday(self, modifier_obj):
+        dict_obj = modifier_obj._data[0]
+        modifier_obj.days_to_birthday(dict_obj, ('days_to_birthday',))
+        error_key = 'days_to_birthday key not set'
+        assert 'days_to_birthday' in dict_obj, error_key
 
 
 def test_password_score():
